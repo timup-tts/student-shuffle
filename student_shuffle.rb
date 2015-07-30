a54829a5ac8a65f651f7b7d01c5a2dcc681febb8
Tim Upchurch <mail@timup.ch>
2015-07-30 10:36:07 -0400

    take user input for student array

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,12 +1,34 @@
-students = ['Tim', 'Daniele', 'Alesia','Anita',
-            'Ben', 'Thomas', 'Matthew',
-            'Mark', 'Jennifer', 'Cameron']
+students = []
+student = ''
+
+puts "Who showed up to class today? Type 'done' when finished:"
+
+while student != 'done'
+  student = gets.chomp
+  students << student
+end
+
+students.pop
 
 students.shuffle!
 
+puts students
+
 count = 0
 
-until count == students.length
-  puts "#{students[count]}, #{students[count + 1]}"
+num_students = students.length
+
+until count == num_students
+  if num_students % 2 == 1
+    if count < num_students - 3
+      puts "#{students[count]}, #{students[count + 1]}"
+    else
+      puts "#{students[count]}, #{students[count + 1]}, #{students[count + 2]}"
+      break
+    end
+  else
+    puts "#{students[count]}, #{students[count + 1]}"
+  end
+
   count += 2
 end
